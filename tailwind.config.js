c456969ff06b97234455c32cebbb22547539ebeb
Yoga Prasetya <yogaajie2@gmail.com>
2024-03-01 09:13:38 +0700

    build: Setup tailwind custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1920px",
+        "4xl": "2560px",
+      },
+    },
+
+    colors: {
+      black: "#000",
+      current: "currentColor",
+      "dark-blue": "#0f172a",
+      green: "#00FF83",
+      transparent: "transparent",
+      white: "#fff",
+    },
+
+    fontFamily: {
+      heading: ["Chakra Petch", "sans-serif"],
+      body: ["Montserrat", "sans-serif"],
+    },
   },
+
   plugins: [],
 };
